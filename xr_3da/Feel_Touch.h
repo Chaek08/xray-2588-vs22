46f15218b439fe1451b03a5db22941b554bb0457
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-27 13:02:20 +0300

    фил тач из релиза

--- xr_3da/Feel_Touch.h
+++ xr_3da/Feel_Touch.h
@@ -7,16 +7,18 @@ namespace Feel
 {
 	class ENGINE_API Touch: private pure_relcase
 	{
-	private:
+	public:
 		struct DenyTouch
 		{
 			CObject*	O;
 			DWORD		Expire;
 		};
+	protected:
 		xr_vector<DenyTouch>	feel_touch_disable;
 
 	public:
 		xr_vector<CObject*>		feel_touch;
+		xr_vector<CObject*>		q_nearest;
 
 	public:
 		void __stdcall			feel_touch_relcase			(CObject* O);
