0440baf5cf756bb68c84fb1731ff56c855f50b64
Chaek08 <chaek777zz@gmail.com>
2025-09-04 20:41:42 +0300

    g_spawn принимает кол-во спавн предметов

--- xr_3da/xrGame/console_commands.cpp
+++ xr_3da/xrGame/console_commands.cpp
@@ -123,38 +123,55 @@ BOOL g_use_scripts_in_goap = 1;
 CUIOptConCom g_OptConCom;
 
 // console commands
-class CCC_Spawn : public IConsole_Command
-{
+class CCC_Spawn : public IConsole_Command {
 public:
-	CCC_Spawn(LPCSTR N) : IConsole_Command(N)  { };
-	virtual void Execute(LPCSTR args) {
-		R_ASSERT(g_pGameLevel);
+    CCC_Spawn(LPCSTR N) : IConsole_Command(N) { };
 
-#ifndef	DEBUG
-		if (GameID() != GAME_SINGLE) 
+    virtual void Execute(LPCSTR args) {
+        R_ASSERT(g_pGameLevel);
+
+    #ifndef DEBUG
+        if (GameID() != GAME_SINGLE)
 		{
-			Msg("For this game type entity-spawning is disabled.");
-			return;
-		};
-#endif
-		char	Name[128];	Name[0]=0;
-		sscanf	(args,"%s", Name);
+            Msg("For this game type entity-spawning is disabled.");
+            return;
+        };
+    #endif
 
-		if (!pSettings->section_exist(Name)) 
+        char Name[128]; 
+        Name[0] = 0;
+        int count = 1;
+
+        int num = sscanf(args, "%s %d", Name, &count);
+
+        if (num < 1)
 		{
-			Msg("! Cannot spawn: section [%s] not found.", Name);
-			return;
-		}
+            Msg("! Invalid arguments. Usage: g_spawn <section> [count]");
+            return;
+        }
 
-		Fvector pos = Actor()->Position();
-		pos.y		+= 2.0f;
-		Level().g_cl_Spawn	(Name,0xff,M_SPAWN_OBJECT_LOCAL, pos);
-	}
-	virtual void	Info	(TInfo& I)		
-	{
-		strcpy(I,"name,team,squad,group"); 
-	}
+        if (!pSettings->section_exist(Name))
+		{
+            Msg("! Cannot spawn: section [%s] not found.", Name);
+            return;
+        }
+
+        for (int i = 0; i < count; ++i)
+		{
+            Fvector pos = Actor()->Position();
+            pos.y += 2.0f;
+
+            Level().g_cl_Spawn(Name, 0xff, M_SPAWN_OBJECT_LOCAL, pos);
+        }
+    }
+
+    virtual void Info (TInfo& I) {
+        strcpy(I,"name [count]");
+    }
 };
+
+
+
 class CCC_GameDifficulty : public CCC_Token {
 public:
 	CCC_GameDifficulty(LPCSTR N) : CCC_Token(N,(u32*)&g_SingleGameDifficulty,difficulty_type_token)  {};
