c3eae41a092b15ed9493479742c87e56b938f0b4
Chaek08 <chaek777zz@gmail.com>
2025-11-05 20:24:44 +0300

    фикс операторов из 👇
    https://github.com/St4lker0k765/xray-1472-vs22/commit/3893cf38807de5deaef1b5e95ca3f07475c6f087

--- xrCore/xrMemory.h
+++ xrCore/xrMemory.h
@@ -55,13 +55,12 @@ IC void*	xr_realloc	(void* P, size_t size)	{	return Memory.mem_realloc(P,size);	
 
 XRCORE_API	char* 	xr_strdup	(const char* string);
 
-#	if !(defined(__BORLANDC__) || defined(NO_XRNEW))
-	IC void*	operator new		(size_t size)		{	return Memory.mem_alloc(size?size:1);				}
-	IC void		operator delete		(void *p)			{	xr_free(p);											}
-	IC void*	operator new[]		(size_t size)		{	return Memory.mem_alloc(size?size:1);				}
-	IC void		operator delete[]	(void* p)			{	xr_free(p);											}
-#	endif
-
+#if !(defined(__BORLANDC__) || defined(NO_XRNEW))
+extern void* __cdecl operator new(size_t size);
+extern void __cdecl	operator delete(void* p) noexcept;
+extern void* __cdecl operator new[](size_t size);
+extern void __cdecl operator delete[](void* p, size_t size) noexcept;
+#endif
 
 // POOL-ing
 const		u32			mem_pools_count			=	54;
